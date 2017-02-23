0ae7c0cf195e2b4fc4869d3a1223f8a75c473ce6
oh <w.juthatip@gmail.com>
2017-02-23 16:59:28 +0700

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_MESSAGE } from './types';
 
 const ROOT_URL = 'http://localhost:3090'
 
@@ -12,6 +12,7 @@ export function signinUser({ email, password }) {
           // If request is good...
           // - Update state to indicate user is authenticated
           dispatch({ type: AUTH_USER });
+          console.log( dispatch({ type: AUTH_USER }))
           // - Save the JWT token
           localStorage.setItem('token', response.data.token);
           // - redirect to the route /'feature'
@@ -51,4 +52,18 @@ export function signoutUser() {
   localStorage.removeItem('token');
   
   return { type: UNAUTH_USER };
+}
+
+export function fetchMessage() {
+  return function(dispatch) {
+    axios.get(ROOT_URL, {
+      headers: { authorization: localStorage.getItem('token') }
+    })
+      .then(response => {
+        dispatch({
+          type: FETCH_MESSAGE,
+          payload: response.data.message
+        });
+      });
+  }
 }
